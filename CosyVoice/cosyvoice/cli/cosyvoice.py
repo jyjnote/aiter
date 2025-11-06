74f40c8f7876b281789f7dd47bcdb024cae3bd97
Jaeyeon <144209498+jyjnote@users.noreply.github.com>
2025-11-06 10:39:25 +0900

    Add kwargs support to inference_instruct2 method

--- CosyVoice/cosyvoice/cli/cosyvoice.py
+++ CosyVoice/cosyvoice/cli/cosyvoice.py
@@ -181,14 +181,16 @@ class CosyVoice2(CosyVoice):
     def inference_instruct(self, *args, **kwargs):
         raise NotImplementedError('inference_instruct is not implemented for CosyVoice2!')
 
-    def inference_instruct2(self, tts_text, instruct_text, prompt_speech_16k, zero_shot_spk_id='', stream=False, speed=1.0, text_frontend=True):
-        assert isinstance(self.model, CosyVoice2Model), 'inference_instruct2 is only implemented for CosyVoice2!'
-        for i in tqdm(self.frontend.text_normalize(tts_text, split=True, text_frontend=text_frontend)):
-            model_input = self.frontend.frontend_instruct2(i, instruct_text, prompt_speech_16k, self.sample_rate, zero_shot_spk_id)
-            start_time = time.time()
-            logging.info('synthesis text {}'.format(i))
-            for model_output in self.model.tts(**model_input, stream=stream, speed=speed):
-                speech_len = model_output['tts_speech'].shape[1] / self.sample_rate
-                logging.info('yield speech len {}, rtf {}'.format(speech_len, (time.time() - start_time) / speech_len))
-                yield model_output
+    def inference_instruct2(self, tts_text, instruct_text, prompt_speech_16k, zero_shot_spk_id='', stream=False, speed=1.0, text_frontend=True, **kwargs): # [수정 1] **kwargs 추가
+            assert isinstance(self.model, CosyVoice2Model), 'inference_instruct2 is only implemented for CosyVoice2!'
+            for i in tqdm(self.frontend.text_normalize(tts_text, split=True, text_frontend=text_frontend)):
+                model_input = self.frontend.frontend_instruct2(i, instruct_text, prompt_speech_16k, self.sample_rate, zero_shot_spk_id)
                 start_time = time.time()
+                logging.info('synthesis text {}'.format(i))
+                
+                # [수정 2] self.model.tts 호출 시 **kwargs를 전달
+                for model_output in self.model.tts(**model_input, stream=stream, speed=speed, **kwargs):
+                    speech_len = model_output['tts_speech'].shape[1] / self.sample_rate
+                    logging.info('yield speech len {}, rtf {}'.format(speech_len, (time.time() - start_time) / speech_len))
+                    yield model_output
+                    start_time = time.time()
